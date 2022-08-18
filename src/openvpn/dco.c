2558dfd1e32e51392ceef905d0c9dfb19449e506
Lev Stipakov <lev@openvpn.net>
2022-08-18 16:44:19 +0200

    dco-win: support for --persist-tun
    Since version 0.8.0, dco-win driver added support for
    DEL_PEER command, which enabled --persist-tun
    implementation on client side.
    
    Add real implementation for dco_del_peer on Windows,
    which calls DEL_PEER, which clears peer state
    on the driver without tearing tunnel down.
    
    Signed-off-by: Lev Stipakov <lev@openvpn.net>

--- src/openvpn/dco.c
+++ src/openvpn/dco.c
@@ -229,11 +229,6 @@ dco_check_option_conflict_platform(int msglevel, const struct options *o)
         return false;
     }
 
-    if (o->persist_tun)
-    {
-        msg(msglevel, "--persist-tun is not supported with ovpn-dco-win.");
-        return false;
-    }
 #elif defined(TARGET_LINUX)
     /* if the device name is fixed, we need to check if an interface with this
      * name already exists. IF it does, it must be a DCO interface, otherwise
